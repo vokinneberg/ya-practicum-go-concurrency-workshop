45706ac025c08735195def6645b1403e788dae0b
vokinneberg <eugene.grebennikov@nbcuni.com>
2024-08-24 19:21:50 -0400

    Add concurrent feed printing

--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -1,6 +1,8 @@
 package crawler
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Crawler struct {
 	feeds []string
@@ -19,6 +21,10 @@ func (c *Crawler) AddFeed(feed string) {
 // Start starts the crawler
 func (c *Crawler) Start() {
 	for _, feed := range c.feeds {
-		fmt.Printf("Crawling %s\n", feed)
+		// Здесь мы используем анонимную функцию, чтобы передать feed внутрь неё и напечатать его.
+		// Почему програма ничего не печатает?
+		go func(f string) {
+			fmt.Printf("Crawling %s\n", f)
+		}(feed)
 	}
 }
