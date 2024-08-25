6039ca0fa0ce098a61eade3c1dbe2b7ccba1934c
vokinneberg <eugene.grebennikov@nbcuni.com>
2024-08-24 21:42:09 -0400

    Change Mutex to RWMutex

--- internal/feed/storage.go
+++ internal/feed/storage.go
@@ -21,7 +21,9 @@ type Storage struct {
 	feeds map[string][]Item
 
 	// Мьютекс для безопасного доступа к данным RSS-лент.
-	m sync.Mutex
+	// Мьютекс позволяет только одной горутине получить доступ к данным RSS-лент.
+	// RWLock позволяет нескольким горутинам получить доступ к данным RSS-лент на чтение. Но только одной горутине на запись.
+	m sync.RWMutex
 }
 
 // NewFeedStorage создает новое хранилище RSS-лент.
@@ -45,8 +47,8 @@ func (fs *Storage) SetFeed(url string, feed []Item) {
 
 // GetFeedLinks возвращает список URL RSS-лент.
 func (fs *Storage) GetLinks() []string {
-	fs.m.Lock()
-	defer fs.m.Unlock()
+	fs.m.RLock()
+	defer fs.m.RUnlock()
 	links := make([]string, 0, len(fs.feeds))
 	for link := range fs.feeds {
 		links = append(links, link)
@@ -56,8 +58,8 @@ func (fs *Storage) GetLinks() []string {
 
 // GetFeed возвращает данные RSS-ленты по URL.
 func (fs *Storage) GetFeed(url string) ([]Item, error) {
-	fs.m.Lock()
-	defer fs.m.Unlock()
+	fs.m.RLock()
+	defer fs.m.RUnlock()
 	if _, ok := fs.feeds[url]; !ok {
 		return nil, ErrFeedNotFound{URL: url}
 	}
@@ -66,8 +68,8 @@ func (fs *Storage) GetFeed(url string) ([]Item, error) {
 
 // GetFeeds возвращает данные всех RSS-лент.
 func (fs *Storage) GetFeeds() []Item {
-	fs.m.Lock()
-	defer fs.m.Unlock()
+	fs.m.RLock()
+	defer fs.m.RUnlock()
 	result := make([]Item, 0, len(fs.feeds))
 	for _, feed := range fs.feeds {
 		if feed != nil {
